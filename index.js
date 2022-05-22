9596be8c38b8fd042d7db7c9d1b7044e11a81106
developer-sahadat <mdridoysikder862@gmail.com>
2022-05-23 01:26:01 +0600

    all services find api added

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 require("dotenv").config();
-var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ys4hh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 /*****Express js mongodb crud operations start code*****/
+
+async function run() {
+  try {
+    await client.connect();
+    /**MongoDb all collection code**/
+    const servicesCollection = client
+      .db("construction_tools")
+      .collection("services");
+
+    /**all services find api code start**/
+
+    app.get("/services", async (req, res) => {
+      const services = await servicesCollection.find({}).toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 /*****Express js mongodb crud operations end code*****/
 
